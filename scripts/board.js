d290394ea6f6aedf3b51602469bee8190612dd5e
Jeff Leep <jdleep@protonmail.com>
2024-08-21 13:12:27 -0500

    Add logic to set cell of game board

--- scripts/board.js
+++ scripts/board.js
@@ -5,7 +5,7 @@ function Gameboard() {
     const sideLength = 3;
     const allowedCellVals = ['X', 'O', ''];
     const initBoard = () => Util.init2dArray(sideLength, sideLength, () => '');
-    const board = initBoard();
+    let board = initBoard();
 
     const save = (brd) => {
         if (isBoardValid(brd)) {
@@ -15,15 +15,24 @@ function Gameboard() {
         return false;
     };
 
+    const setCell = (x, y, val) => brd => {
+        if (Util.areCoordsValid(x, y, brd)) {
+            const newBrd = R.clone(brd);
+            newBrd[x][y] = val;
+            return newBrd;
+        }
+        return false;
+    };
+
     const getBoard = () => R.clone(board);
 
     const isBoardValid = (brd) => {
         return Array.isArray(brd)
         && Util.isSquareOfLen(sideLength, brd)
-        && Util.onlyContainsVals2d(allowedCellVals, brd);
+        && Util.onlyContainsVals2d(brd);
     };
 
-    return { save, getBoard, initBoard };
+    return { save, getBoard, initBoard, setCell };
 }
 
 export { Gameboard };
--- tests/board.test.js
+++ tests/board.test.js
@@ -6,3 +6,23 @@ test('Initialize board', () => {
                                       ['','',''],
                                       ['','','']]);
 });
+
+
+test('Save board - output', () => {
+    const gameBoard = Board.Gameboard();
+    const saveBoard = gameBoard.setCell(1, 2, 'X')(gameBoard.getBoard());
+    console.log(saveBoard);
+    expect(gameBoard.save(saveBoard)).toEqual([['','',''],
+                                               ['','','X'],
+                                               ['','','']]);
+});
+
+test('Save board - getBoard() - whether saved to object', () => {
+    const gameBoard = Board.Gameboard();
+    const saveBoard = gameBoard.setCell(1, 2, 'X')(gameBoard.getBoard());
+    console.log(saveBoard);
+    gameBoard.save(saveBoard);
+    expect(gameBoard.getBoard()).toEqual([['','',''],
+                                               ['','','X'],
+                                               ['','','']]);
+});
