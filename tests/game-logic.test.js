4d2afed18ccdfb51f560520a072b16a685b09707
Jeff Leep <jdleep@protonmail.com>
2024-09-10 16:40:37 -0500

    Initialize gameLogic outside of tests

--- tests/game-logic.test.js
+++ tests/game-logic.test.js
@@ -3,6 +3,7 @@ import { Gameboard } from '../scripts/board';
 import * as R from 'ramda';
 
 let board;
+const gameLogic = GameLogic();
 
 beforeEach(() => {
     board = Gameboard(3, ['X', 'O']);
@@ -14,8 +15,6 @@ test('isWinner - X wins', () => {
                         .setCell(1,1,'X')
                         .setCell(2,2,'X');
 
-    const gameLogic = GameLogic();
-
     expect(gameLogic.isWinner({token: 'X'}, newBoard.getBoard())).toBe(true);
 });
 
@@ -24,8 +23,6 @@ test('isWinner - X is not winner', () => {
     const newBoard = board.setCell(0,0,'X')
                         .setCell(1,1,'X')
                         .setCell(2,2,'O');
-
-    const gameLogic = GameLogic();
 
     expect(gameLogic.isWinner({token: 'X'}, newBoard.getBoard())).toBe(false);
 });
