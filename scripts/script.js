b325c5f94db3d5e60275740ba38ef096cfaa5243
Jeff Leep <jdleep@protonmail.com>
2024-09-18 16:24:54 -0500

    Add game status display

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ const domController = (function() {
     const cellArray = generateCellArr(3);
     setDomGameBoard(gameBoardDiv, cellArray);
     setCellListeners(cellArray);
+    const statusMsg = document.querySelector('.game-status');
 
     const gameController = GameController.GameController();
 
@@ -25,6 +26,29 @@ const domController = (function() {
         div.replaceChildren(...cellArr.flat());
     }
 
+    // todo make more flexible based on player 1 / player 2 info
+    function createStatusMsg(status) {
+        let msg;
+        switch(status) {
+            case 'X':
+                msg = 'Player 1 wins!';
+                break;
+            case 'O':
+                msg = 'Player 2 wins!';
+                break;
+            case 'TIE':
+                msg = 'Game is tied!';
+                break;
+            case 'IN PROGRESS':
+                msg = 'Game is still in progress!';
+                break;
+            default:
+                msg = 'Error';
+        }
+
+        return msg;
+    }
+
     // Todo - figure out consistent x / y notation
     function setCellListeners(cellArr) {
         cellArr.forEach((row, rowInd) => {
@@ -32,6 +56,8 @@ const domController = (function() {
                 el.addEventListener("click", () => {
                     gameController.move(rowInd, colInd);
                     el.innerText = gameController.getCell(rowInd, colInd);
+                    statusMsg.innerText = 
+                        createStatusMsg(gameController.getGameStatus());
                 })
             })
         });
